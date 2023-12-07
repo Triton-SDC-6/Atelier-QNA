6a1f3b350e9b3ae0b27e99e16a25277f4f5128c8
Xianyi Liang <liangxianyi2011@gmail.com>
2023-12-07 01:29:54 -0800

    implement basic findAll

--- src/models/Questions.js
+++ src/models/Questions.js
@@ -8,11 +8,50 @@ const pool = new Pool({
 });
 
 async function getAll(productId, page, count) {
-  const queryString = `SELECT * FROM questions WHERE product_id=${productId} LIMIT ${count} OFFSET ${
+  const questionsQuery =
+    'SELECT * FROM questions WHERE product_id = $1 AND reported = FALSE LIMIT $2 OFFSET $3';
+  const questions = await pool.query(questionsQuery, [
+    productId,
+    count,
     (page - 1) * count
-  }`;
-  const result = await pool.query(queryString);
-  return result.rows;
+  ]);
+  const questionIds = questions.rows.map(question => question.id);
+  const answersQuery = 'SELECT * FROM answers WHERE question_id = ANY($1)';
+  const answers = await pool.query(answersQuery, [questionIds]);
+  const answersByQuestion = answers.rows.reduce((acc, answer) => {
+    if (!acc[answer.question_id]) {
+      acc[answer.question_id] = [];
+    }
+    acc[answer.question_id].push(answer);
+    return acc;
+  }, {});
+
+  const answerIds = answers.rows.map(answer => answer.id);
+  const photosQuery = 'SELECT * FROM answers_photos WHERE answer_id = ANY($1)';
+  const photos = await pool.query(photosQuery, [answerIds]);
+  const photosByAnswer = photos.rows.reduce((acc, photo) => {
+    if (!acc[photo.answer_id]) {
+      acc[photo.answer_id] = [];
+    }
+    acc[photo.answer_id].push(photo);
+    return acc;
+  }, {});
+
+  const combinedResults = questions.rows.map(question => {
+    const answersObject = (answersByQuestion[question.id] || []).reduce(
+      (obj, answer) => {
+        obj[answer.id] = {
+          ...answer,
+          photos: photosByAnswer[answer.id] || []
+        };
+        return obj;
+      },
+      {}
+    );
+    return { ...question, answers: answersObject };
+  });
+
+  return combinedResults;
 }
 
 module.exports.getAll = getAll;
