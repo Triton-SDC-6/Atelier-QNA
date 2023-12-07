7005b60b93b146161cc2502cd1ee7a3e53e02cac
Xianyi Liang <liangxianyi2011@gmail.com>
2023-12-07 00:10:58 -0800

    pudate controller to be able to get query

--- src/controllers/questionsControllers.js
+++ src/controllers/questionsControllers.js
@@ -1,7 +1,8 @@
 const Questions = require('../models/Questions');
 
 async function getAll(req, res) {
-  const data = await Questions.getAll(1, 1, 5);
+  const { product_id, page=1, count=2 } = req.query;
+  const data = await Questions.getAll(product_id, page, count);
   res.send(data);
 }
 
