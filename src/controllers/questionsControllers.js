1eaa8ddd3ebaf25a2da2ebc65efe596f99fb02f2
Xianyi Liang <liangxianyi2011@gmail.com>
2023-12-07 13:13:59 -0800

    set defual count and page for both q and a

--- src/controllers/questionsControllers.js
+++ src/controllers/questionsControllers.js
@@ -2,14 +2,15 @@ const Questions = require('../models/Questions');
 const Answers = require('../models/Answers');
 
 async function getAll(req, res) {
-  const { product_id, page = 1, count = 2 } = req.query;
+  const { product_id, page = 1, count = 5 } = req.query;
   const data = await Questions.getAll(product_id, page, count);
   res.send(data);
 }
 
 async function getAllAnswersOfQuestion(req, res) {
   const { question_id } = req.params;
-  const data = await Answers.getAllByQuestionId(question_id);
+  const { page = 1, count = 5 } = req.query;
+  const data = await Answers.getAllByQuestionId(question_id, page, count);
   res.send(data);
 }
 module.exports.getAll = getAll;
--- src/models/Answers.js
+++ src/models/Answers.js
@@ -1,8 +1,13 @@
 const { pool } = require('./Questions');
 
-async function getAllByQuestionId(questionId) {
-  const answersQuery = 'SELECT * FROM answers WHERE question_id = $1';
-  const answers = await pool.query(answersQuery, [questionId]);
+async function getAllByQuestionId(questionId, page, count) {
+  const answersQuery =
+    'SELECT * FROM answers WHERE question_id = $1 LIMIT $2 OFFSET $3';
+  const answers = await pool.query(answersQuery, [
+    questionId,
+    count,
+    (page - 1) * count
+  ]);
   return answers.rows;
 }
 
