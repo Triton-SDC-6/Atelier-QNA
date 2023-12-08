fc69909d3767fb10c7aaa7adbaaac1aedd8c5e92
Xianyi Liang <liangxianyi2011@gmail.com>
2023-12-07 19:46:27 -0800

    Fix answer that only show not reported

--- src/models/Answers.js
+++ src/models/Answers.js
@@ -2,7 +2,7 @@ const { pool } = require('./Questions');
 
 async function getAllByQuestionId(questionId, page, count) {
   const answersQuery =
-    'SELECT * FROM answers WHERE question_id = $1 LIMIT $2 OFFSET $3';
+    'SELECT * FROM answers WHERE question_id = $1 AND reported = FALSE LIMIT $2 OFFSET $3';
   const answers = await pool.query(answersQuery, [
     questionId,
     count,
